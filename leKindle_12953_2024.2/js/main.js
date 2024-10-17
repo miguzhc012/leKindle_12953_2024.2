4f45de68bc64830f34d364291bd2baf3ee400c39
miguzhc012 <migups8@gmail.com>
2024-10-16 21:45:50 -0300

    aula 08

--- leKindle_12953_2024.2/js/main.js
+++ leKindle_12953_2024.2/js/main.js
@@ -7,7 +7,7 @@ function createNavebar(){
             <li><a href="index.html#cards">Galeria</a></li>
             <li><a href="index.html#agenda">Agenda</a></li>
             <li><a href="index.html#contato">Contato</a></li>
-            <li><a href="login.html">Entrar</a></li>
+            <li><a href="login.html#">Entrar</a></li>
         </ul>
     </nav>
     `
